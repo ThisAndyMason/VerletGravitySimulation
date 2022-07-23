b59d992c8b2a1c332383b5527e7768fcaad2bb0b
andy <andy_mason@hotmail.com>
2022-07-23 10:43:32 +0100

    dont recreate objects each run

--- main.py
+++ main.py
@@ -43,28 +43,35 @@ class Planet:
         self.radius = radius
         self.trail_x = Kew()
         self.trail_y = Kew()
+        self.shape = pyglet.shapes.Circle(
+            x=self.x, y=self.y, radius=self.radius, color=(250, 25, 30)
+        )
+
+    def init_draw(self):
+        self.shape.draw()
 
     def draw(self):
-        pyglet.shapes.Circle(
-            x=self.x, y=self.y, radius=self.radius, color=(250, 25, 30)
-        ).draw()
+        self.shape.x = self.x
+        self.shape.y = self.y
+        self.shape.draw()
 
-        for i in range(len(self.trail_x)):
-            j = i - 1
-            if i - 1 < 0:
-                j = i
-            if not (
-                abs(self.trail_x[i] - self.trail_x[j]) > (width / 100)
-                or abs(self.trail_y[i] - self.trail_y[j]) > (height / 100)
-            ):
 
-                pyglet.shapes.Line(
-                    x=self.trail_x[i],
-                    y=self.trail_y[i],
-                    x2=self.trail_x[j],
-                    y2=self.trail_y[j],
-                    color=(250, 25, 30),
-                ).draw()
+        # for i in range(len(self.trail_x)):
+        #     j = i - 1
+        #     if i - 1 < 0:
+        #         j = i
+        #     if not (
+        #         abs(self.trail_x[i] - self.trail_x[j]) > (width / 100)
+        #         or abs(self.trail_y[i] - self.trail_y[j]) > (height / 100)
+        #     ):
+        #
+        #         pyglet.shapes.Line(
+        #             x=self.trail_x[i],
+        #             y=self.trail_y[i],
+        #             x2=self.trail_x[j],
+        #             y2=self.trail_y[j],
+        #             color=(250, 25, 30),
+        #         ).draw()
 
     def update_position(self):
         self.trail_x.append(self.x)
@@ -91,31 +98,18 @@ class Planet:
 
 
 window = pyglet.window.Window(height, width, resizable=True, vsync=True)
-planet1 = Planet(50, 50, 90, 5, 10, 10)
-planet2 = Planet(450, 450, 30, 10, 15, 20)
-planets = [planet1, planet2]
+planets = []
 
-for i in range(300):
-    planets.append(Planet(random.randrange(0,width),random.randrange(0,height),random.randrange(-150,150),random.randrange(-150,150),random.randrange(5,12),random.randrange(5,12)))
+for i in range(50):
+    planets.append(Planet(random.randrange(0,width),random.randrange(0,height),random.randrange(-150,150),random.randrange(-150,150),2,2))
 
 
 
-class Tick:
-    def __init__(self):
-        self.tick = 0
-
-    def increment_tick(self):
-        self.tick = self.tick + 1
-
-    def get_tick(self):
-        return self.tick
-
-
-
+for planet in planets:
+    planet.init_draw()
 
 def update(_):
     window.clear()
-
 
     for planet in planets:
         planet.update_position()
